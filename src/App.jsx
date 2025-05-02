5b425aabeb64335831f43e93ae07ca61ecec4109
swarnaliiiiii <oli2k3@gmail.com>
2025-05-02 12:06:55 +0530

    Add pulsating text glow effect and update button styles for enhanced UI

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <h1 className="text-2xl font-semibold text-white mb-6 ml-8 pt-2">Jibly.ai</h1>
         <div className="absolute right-0 top-0 w-[500px] h-[500px] bg-gradient-to-br from-purple-500 via-pink-500 to-blue-500 opacity-40 blur-3xl rounded-full pointer-events-none z-0" />
         <div className="absolute left-0 bottom-0 w-[500px] h-[500px] bg-gradient-to-br from-purple-500 via-pink-500 to-blue-500 opacity-40 blur-3xl rounded-full pointer-events-none z-0" />
-        <button className="rounded-full text-glow bg-pink-500 text-white border border-[#e5e5e5] mt-3 mr-13 px-4 py-2 absolute top-4 right-4 hover:bg-[#e5e5e5] hover:text-black transition duration-300 ease-in-out shadow-lg shadow-pink-500/90">
+        <button className="rounded-full text-glow-pulse bg-pink-500 text-white border border-[#e5e5e5] mt-3 mr-13 px-4 py-2 absolute top-4 right-4 hover:bg-[#e5e5e5] hover:text-black transition duration-300 ease-in-out">
           Sign Up
         </button>
 
@@ -27,10 +27,10 @@ function App() {
           <p className='text-lg font-light sm:text-xl text-gray-300 mt-4'>
             Analyzes live job listings and your resume in real-time to show your eligibility, improvement areas, and even writes custom cover letters — instantly.
           </p>
-          <button className='mt-8 bg-pink-500 text-glow text-white px-6 py-3 rounded-full border border-[#e5e5e5] hover:bg-[#e5e5e5] hover:text-black transition duration-300'>
+          <button className='mt-8 bg-pink-500 text-glow-pulse text-white px-6 py-3 rounded-full border border-[#e5e5e5] hover:bg-[#e5e5e5] hover:text-black transition duration-300'>
             Get Started
           </button>
-          <button className='mt-8 mr-4 ml-3 bg-pink-500 text-glow text-white px-6 py-3 rounded-full border border-[#e5e5e5] hover:bg-[#e5e5e5] hover:text-black transition duration-300'>
+          <button className='mt-8 mr-4 ml-3 bg-grey-500 text-glow-pulse text-white px-6 py-3 rounded-full border border-[#e5e5e5] hover:bg-[#e5e5e5] hover:text-black transition duration-300'>
             Learn More
           </button>
         </div>
