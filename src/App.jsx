fe341521c306fd66254bb59c5c4078bdd31cf786
swarnaliiiiii <oli2k3@gmail.com>
2025-05-02 10:31:38 +0530

    Refactor App component layout and update button styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,15 @@ function App() {
 
   return (
     <>
-      <h1 className='text-2xl font-bold m-8 pl-10'>Jibly-Context</h1>
-      <button className='rounded-full bg-black text-white border border-[#e5e5e5] mt-3 mr-8 px-4 py-2 absolute top-4 right-4 hover:bg-[#e5e5e5] hover:text-black transition duration-300 ease-in-out'>
+      <div className="flex min-h-screen bg-gradient-to-tr from-blue-950 from-60% to-emerald-500 to-90% ">
+      <h1 className='text-2xl font-bold m-8 pl-19 text-white'>Jibly.ai</h1>
+      <button className='rounded-full bg-pink text-white border border-[#e5e5e5] mt-3 mr-13 px-4 py-2 absolute top-4 right-4 hover:bg-[#e5e5e5] hover:text-black transition duration-300 ease-in-out'>
          Sign Up
       </button>
+      <button className='flex flex-col items-center bg-p'>
+        Welcome to Jibly.ai
+      </button>
+      </div>
     </> 
   )
 }
